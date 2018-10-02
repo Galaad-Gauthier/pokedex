28d5f10d078815386e3c28d4b49a853ae830c718
galaad-gauthier <coontail7@gmail.com>
2018-10-02 14:52:16 +0200

    Fixed stuff

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -4,12 +4,6 @@ class TelegramController < Telegram::Bot::UpdatesController
 
   TELEGRAM_COMMAND_REGEXP = /^\/\w+ (.+)$/.freeze
 
-  expose(:payload)      { self.payload }
-  expose(:command_args) { payload["text"].match(TELEGRAM_COMMAND_REGEXP).try(:[], 1).try(:split, ' ') }
-  expose(:tag)          { command_args.first }
-  expose(:body)         { command_args.last }
-  expose(:info)         { Info.where(tag: tag).first_or_initialize }
-
   before_action :authorize_chat_id
   before_action :ensure_args_presence
 
@@ -18,32 +12,48 @@ class TelegramController < Telegram::Bot::UpdatesController
   end
 
   def add_info!
-    if info.persisted?
-      info.body = body
+    if @info.persisted?
+      @info.body = body
     else
-      info.body += "\n\n#{body}"
+      @info.body += "\n\n#{body}"
     end
 
     reply_with :message, text: "The infos about #{tag} were successfully updated."
   end
 
   def clear_info!
-    info.destroy!
+    @info.destroy!
     reply_with :message, text: "All infos about #{tag} were successfully destroyed."
   end
 
   def info!
-    reply_with :message, text: info.body
+    reply_with :message, text: @info.body
   end
 
   private
 
   def authorize_chat_id
-    head :forbidden unless payload['chat']['id'] == ENV['POKEDEX_CHAT_ID']
+    head :forbidden unless self.payload['chat']['id'] == ENV['POKEDEX_CHAT_ID']
   end
 
   def ensure_args_presence
     head :bad_request unless tag && body
+  end
+
+  def command_args
+    @command_args ||= self.payload["text"].match(TELEGRAM_COMMAND_REGEXP).try(:[], 1).try(:split, ' ')
+  end
+
+  def tag
+    @tag ||= command_args.first
+  end
+
+  def body
+    @body ||= command_args.last
+  end
+
+  def info
+    @info ||= Info.where(tag: tag).first_or_initialize
   end
 
 end
